60a37943acc42a5ee6dc6f83b7708868f8541e3a
ymzhang0 <zhangyiming1998@gmail.com>
2026-05-26 15:03:35 +0200

    feat: port updates to process analyzer

--- src/aiida_thermo_pw/tools/analyser.py
+++ src/aiida_thermo_pw/tools/analyser.py
@@ -1,5 +1,4 @@
 from re import S
-from tkinter import N
 from aiida import orm
 from aiida.common.links import LinkType
 from aiida.engine import ProcessState
@@ -7,9 +6,6 @@ from enum import Enum
 from collections import OrderedDict
 from abc import ABC, abstractmethod
 from rich import print as rprint
-import numpy
-from aiida_analyser import BaseWorkChainAnalyser
-
 class ThermoPwBaseWorkChainState(Enum):
     """
     Analyser for the ThermoPwBaseWorkChain.
@@ -24,7 +20,7 @@ class ThermoPwBaseWorkChainState(Enum):
     ERROR_NSTEP = 7
     UNKNOWN = 999
 
-class ThermoPwBaseAnalyser(BaseWorkChainAnalyser):
+class ThermoPwBaseAnalyser:
     """
     Analyser for the ThermoPwBaseWorkChain.
     """
@@ -33,7 +29,7 @@ class ThermoPwBaseAnalyser(BaseWorkChainAnalyser):
     ])
 
     def __init__(self, workchain: orm.WorkChainNode):
-        super().__init__(workchain)
+        self.node = workchain
         self.state = ThermoPwBaseWorkChainState.UNKNOWN
         self.descendants = {}
         for link_label, _ in self._all_descendants.items():
@@ -50,209 +46,81 @@ class ThermoPwBaseAnalyser(BaseWorkChainAnalyser):
                 iterations.append(node)
         return iterations
 
-    # def check_process_state(self):
+    def check_process_state(self):
 
-    #     from collections import deque
-    #     from io import StringIO
+        from collections import deque
+        from io import StringIO
 
-    #     state = ThermoPwBaseWorkChainState.UNKNOWN
-    #     message = ''
+        state = ThermoPwBaseWorkChainState.UNKNOWN
+        message = ''
         
 
-    #     source_db, source_id = self.node.inputs.thermo_pw.structure.base.extras.get_many(('source_db', 'source_id'))
-    #     source = f"{source_db}-{source_id}"
+        source_db, source_id = self.node.inputs.thermo_pw.structure.base.extras.get_many(('source_db', 'source_id'))
+        source = f"{source_db}-{source_id}"
 
-    #     if self.node.process_state == ProcessState.WAITING:
-    #         state = ThermoPwBaseWorkChainState.WAITING
-    #         message = f"{self.node.process_state}"
-    #     elif self.node.process_state == ProcessState.RUNNING:
-    #         state = ThermoPwBaseWorkChainState.RUNNING
-    #         message = f"{self.node.process_state}"
-    #     elif self.node.process_state == ProcessState.EXCEPTED:
-    #         state = ThermoPwBaseWorkChainState.EXCEPTED
-    #         message = f"{self.node.process_state}"
-    #     elif self.node.process_state == ProcessState.FINISHED:
-    #         if self.node.exit_status != 0:
-    #             final_iteration = self.get_iterations()[-1]
-    #             if final_iteration.is_killed:
-    #                 state = ThermoPwBaseWorkChainState.KILLED
-    #                 message = f"{self.node.process_state} killed in final iteration [{final_iteration.pk}]"
-    #                 return state, message
-    #             if final_iteration.is_excepted:
-    #                 state = ThermoPwBaseWorkChainState.EXCEPTED
-    #                 message = f"{self.node.process_state} excepted in final iteration [{final_iteration.pk}]"
-    #                 return state, message
-    #             stderr = final_iteration.get_scheduler_stderr()
-    #             aiida_out = final_iteration.outputs.retrieved.get_object_content("aiida.out")
-    #             for error, error_message in (
-    #                 (ThermoPwBaseWorkChainState.ERROR_TOO_MANY_PROCESSES, 'there are processes with no planes.'),
-    #                 (ThermoPwBaseWorkChainState.ERROR_TIME_LIMIT,  'TIME LIMIT'),
-    #             ):
-    #                 if error_message in stderr:
-    #                     state = error
-    #                     message = f"{error_message} in final iteration [{final_iteration.pk}]"
-    #                     return state, message
-    #             for error, error_message in (
-    #                 (ThermoPwBaseWorkChainState.ERROR_NSTEP, 'Incorrect nstep, check elastic_algorithm'),
-    #             ):
-    #                 if error_message in aiida_out:
-    #                     state = error
-    #                     message = f"{error_message} in final iteration [{final_iteration.pk}]"
-    #                     return state, message
-    #             state = ThermoPwBaseWorkChainState.FINISHED_OK
-    #             message = f"{self.node.process_state} finished with exit status {self.node.exit_status} and final iteration [{final_iteration.pk}]"
-    #             return state, message
-    #         else:
-    #             state = ThermoPwBaseWorkChainState.FINISHED_OK
-    #             message = f"{self.node.process_state} finished with exit status {self.node.exit_status}"
-    #             return state, message
-    #     else:
-    #         state = ThermoPwBaseWorkChainState.UNKNOWN
-    #         message = f"{self.node.process_state} "
-    #         return state, message
-
-    #     return state, message
-
-    def get_source(self):
-        """Get the source of the workchain."""
-        source = super().get_source()
-        if source is None:
-            try:
-                source_db, source_id = self.node.inputs.thermo_pw.structure.base.extras.get_many(('source_db', 'source_id'))
-                source = f"{source_db}-{source_id}"
-            except Exception:
-                print('Source is not set')
-                return None
-        return source
-
-    def print_state(self):
-        """Print the state of the workchain."""
-        result = self.get_state()
-        if not result:
-            print(f"Can't check the state of ThermoPwBaseWorkChain<{self.node.pk}>.")
-            return
-        path, process_state = result
-        print(f"ThermoPwBaseWorkChain<{self.node.pk}> is now {process_state} at {path}.")
-    
-    def get_moduli(self, modulus_type: str):
-        """Get the moduli of the workchain."""
-        if not self.node.is_finished_ok:
-            return None
-        moduli = {
-            average: self.node.outputs.output_parameters.get('moduli').get(average).get(modulus_type) 
-            for average in ['voigt', 'reuss', 'vrh']
-            }
-        return moduli
-
-    @property
-    def code(self):
-        """Get the code of the workchain."""
-        return self.node.inputs.thermo_pw.code
-
-    @property
-    def elastic_constants(self):
-        """Get the elastic constants of the workchain."""
-        if not self.node.is_finished_ok:
-            return None
-        return self.node.outputs.elastic_constants.get_array('elastic_constants')
-
-    @property
-    def bulk_modulus(self):
-        """Get the moduli of the workchain."""
-        return self.get_moduli('bulk_modulus_B')
-
-    @property
-    def young_modulus(self):
-        """Get the Young modulus of the workchain."""
-        return self.get_moduli('young_modulus_E')
-
-    @property
-    def shear_modulus(self):
-        """Get the Shear modulus of the workchain."""
-        return self.get_moduli('shear_modulus_G')
-
-    @property
-    def poisson_ratio(self):
-        """Get the Poisson ratio of the workchain."""
-        return self.get_moduli('poisson_ratio_n')
-
-    @property
-    def pugh_ratio(self):
-        """Get the Pugh ratio of the workchain."""
-        return self.get_moduli('pugh_ratio_r')
-
-    @property
-    def pettifor_ratio(self):
-        """Get the Pettifor ratio of the workchain."""
-        bulk_modulus = self.bulk_modulus
-        # Note that both the elastic constants and the bulk modulus are in kbar.
-        elastic_constants = self.elastic_constants
-        if bulk_modulus is None or elastic_constants is None:
-            return None
-        return {
-            average: (elastic_constants[0][1] - elastic_constants[2][2]) / bulk_modulus[average] for average in ['voigt', 'reuss', 'vrh']
-        }
-
-    @property
-    def modified_pettifor_ratio(self):
-        """Get the modified Pettifor ratio of the workchain."""
-        young_modulus = self.young_modulus
-        elastic_constants = self.elastic_constants
-        if young_modulus is None or elastic_constants is None:
-            return None
-        return {
-            average: (elastic_constants[0][1] - elastic_constants[2][2]) / young_modulus[average] for average in ['voigt', 'reuss', 'vrh']
-        }
-
-    def clean_workchain(self, exempted_states, dry_run: bool = True):
-        """Clean the workchain."""
-        super().clean_workchain(exempted_states, dry_run)
-
-    def get_fitting_coefficients(self):
-        """Get the fitting coefficients of the workchain."""
-        if not self.node.is_finished_ok:
-            return None
-        return self.node.outputs.output_parameters.get('elastic_constants_fitting')
-
-    def plot_elastic_fitting(self, axis=None):
-        """Plot the elastic fitting of the workchain."""
-        if not self.node.is_finished_ok:
-            return None
-        fitting_coefficients = self.get_fitting_coefficients()
-        if not axis:
-            from matplotlib import pyplot as plt
-            fig, ax = plt.subplots(1, 1, figsize=(6, 8))
+        if self.node.process_state == ProcessState.WAITING:
+            state = ThermoPwBaseWorkChainState.WAITING
+            message = f"{self.node.process_state}"
+        elif self.node.process_state == ProcessState.RUNNING:
+            state = ThermoPwBaseWorkChainState.RUNNING
+            message = f"{self.node.process_state}"
+        elif self.node.process_state == ProcessState.EXCEPTED:
+            state = ThermoPwBaseWorkChainState.EXCEPTED
+            message = f"{self.node.process_state}"
+        elif self.node.process_state == ProcessState.FINISHED:
+            if self.node.exit_status != 0:
+                final_iteration = self.get_iterations()[-1]
+                if final_iteration.is_killed:
+                    state = ThermoPwBaseWorkChainState.KILLED
+                    message = f"{self.node.process_state} killed in final iteration [{final_iteration.pk}]"
+                    return state, message
+                if final_iteration.is_excepted:
+                    state = ThermoPwBaseWorkChainState.EXCEPTED
+                    message = f"{self.node.process_state} excepted in final iteration [{final_iteration.pk}]"
+                    return state, message
+                stderr = final_iteration.get_scheduler_stderr()
+                aiida_out = final_iteration.outputs.retrieved.get_object_content("aiida.out")
+                for error, error_message in (
+                    (ThermoPwBaseWorkChainState.ERROR_TOO_MANY_PROCESSES, 'there are processes with no planes.'),
+                    (ThermoPwBaseWorkChainState.ERROR_TIME_LIMIT,  'TIME LIMIT'),
+                ):
+                    if error_message in stderr:
+                        state = error
+                        message = f"{error_message} in final iteration [{final_iteration.pk}]"
+                        return state, message
+                for error, error_message in (
+                    (ThermoPwBaseWorkChainState.ERROR_NSTEP, 'Incorrect nstep, check elastic_algorithm'),
+                ):
+                    if error_message in aiida_out:
+                        state = error
+                        message = f"{error_message} in final iteration [{final_iteration.pk}]"
+                        return state, message
+                state = ThermoPwBaseWorkChainState.FINISHED_OK
+                message = f"{self.node.process_state} finished with exit status {self.node.exit_status} and final iteration [{final_iteration.pk}]"
+                return state, message
+            else:
+                state = ThermoPwBaseWorkChainState.FINISHED_OK
+                message = f"{self.node.process_state} finished with exit status {self.node.exit_status}"
+                return state, message
         else:
-            ax = axis
+            state = ThermoPwBaseWorkChainState.UNKNOWN
+            message = f"{self.node.process_state} "
+            return state, message
 
-        for x, x_info in fitting_coefficients.items():
-            for y, y_info in x_info.items():
-                strains = numpy.array(y_info.get('strains'))
-                stresses = numpy.array(y_info.get('stresses'))
-                coefficients = numpy.array(y_info.get('coefficients'))
-                ax.scatter(strains, stresses, color='blue')
-                # ax.plot(strains, stresses, color='blue', label=f'{x}-{y}')
-                polynomial = numpy.poly1d(coefficients[::-1])
-                ax.plot(strains, 147100*polynomial(strains), color='red', label=f'{x}-{y} fitting')
-        ax.legend(loc='best')  
-        return ax
+        return state, message
 
-    def get_RMS_error(self):
-        """Get the RMS error of the workchain."""
-        if not self.node.is_finished_ok:
-            return None
 
-        RMS_errors = {}
-        fitting_coefficients = self.get_fitting_coefficients()
-        for x, x_info in fitting_coefficients.items():
-            RMS_errors[x] = {}
-            for y, y_info in x_info.items():
-                strains = numpy.array(y_info.get('strains'))
-                stresses = numpy.array(y_info.get('stresses'))
-                coefficients = numpy.array(y_info.get('coefficients'))
-                polynomial = numpy.poly1d(coefficients[::-1])
-                errors = stresses - 147100*polynomial(strains)
-                RMS_error = numpy.sqrt(numpy.mean(errors**2))
-                # print(f'RMS error for {x}-{y} is {RMS_error}')
-                RMS_errors[x][y] = RMS_error
-        return RMS_errors
+    def delete_nodes_and_remote_folder(
+        self,
+    ):
+        from aiida.common import NotExistentAttributeError
+        from aiida.tools import delete_nodes
+
+        for called_descendant in self.node.called_descendants:
+            if isinstance(called_descendant, orm.CalcJobNode):
+                try:
+                    called_descendant.outputs.remote_folder._clean()  # pylint: disable=protected-access
+                except (IOError, OSError, KeyError, NotExistentAttributeError):
+                    pass
+
+        delete_nodes([self.node.pk], dry_run=False)   
